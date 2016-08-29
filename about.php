9ce6c5461d716d6f1a4559f8fd0b2ee93c74c169
keyswor <keyswor1@gmail.com>
2016-08-29 20:44:03 +0545

    spandan design

---
+++ about.php
@@ -0,0 +1,99 @@
+
+	<!DOCTYPE HTML>
+	
+<html>
+	<head>
+		<title>Spandan Design</title>
+		<meta charset="utf-8" />
+		<meta name="viewport" content="width=device-width, initial-scale=1" />
+		<!--[if lte IE 8]><script src="assets/js/ie/html5shiv.js"></script><![endif]-->
+		<link rel="stylesheet" href="assets/css/main.css" />
+		<!--[if lte IE 8]><link rel="stylesheet" href="assets/css/ie8.css" /><![endif]-->
+		<!--[if lte IE 9]><link rel="stylesheet" href="assets/css/ie9.css" /><![endif]-->
+	</head>
+	<body class="landing">
+
+		<!-- Header -->
+			<header id="header" class="alt">
+				<h1><a href="index.php">Spandan Design</a></h1>
+				<a href="#nav">Menu</a>
+			</header>
+
+		<!-- Nav -->
+			<nav id="nav">
+				<ul class="links">
+					<li><a href="index.php">Home</a></li>
+                                        <li><a href="about.php">About</a></li>
+					<li><a href="elements.html">Contact US</a></li>
+				</ul>
+			</nav>
+                <section id="one" class="wrapper style1">
+                            
+                            
+				<div class="inner">
+                                    <p align="center"; style="font-family:Courier; color:Blue; font-size: 40px; padding-top:3px;"><b>WHY US?</b></p>
+					<article class="feature left">
+                                   
+						<div class="content">
+							<h2>Great Ambition</h2>
+							<p>The leadership team of every good company has a great ambition for the company – usually one that addresses an unmet customer need. 
+                                                            The ambition is not one of personal greed; it’s about building a company that delivers on its promise and does it with a unique quality.
+                                                            Our experience over the years is that it takes a great ambition to create even a good company.
+                                                            </p>
+<!--							<ul class="actions">
+								<li>
+									<a href="#" class="button alt">More</a>
+								</li>
+							</ul>-->
+						</div>
+					</article>
+					<article class="feature right">
+                                    
+						<div class="content">
+							<h2>Focus on customer's need</h2>
+							<p>Every good company begins by meeting a customer need. That need is often deeply understood
+                                                            by our team.We deeply understand customer's need, experienced the need – and 
+                                                            see how that need was not being well met.</p>
+							
+						</div>
+					</article>
+                                    <article class="feature left">
+                                        
+						<div class="content">
+							<h2>Dedicated Team</h2>
+							<p> All member of our team are very creative and always focus on customers satisfaction.
+                                                         </p>
+<!--							<ul class="actions">
+								<li>
+									<a href="#" class="button alt">More</a>
+								</li>
+							</ul>-->
+						</div>
+					</article>
+				</div>
+			</section>
+                <footer id="footer">
+				<div class="inner">
+					<ul class="icons">
+						<li><a href="#" class="icon fa-facebook">
+							<span class="label">Facebook</span>
+						</a></li>
+						<li><a href="#" class="icon fa-twitter">
+							<span class="label">Twitter</span>
+						</a></li>
+						
+					</ul>
+					<ul class="copyright">
+						<li>&copy; kishor.</li>
+						
+					</ul>
+				</div>
+			</footer>
+        </body>
+        <!-- Scripts -->
+			<script src="assets/js/jquery.min.js"></script>
+			<script src="assets/js/skel.min.js"></script>
+			<script src="assets/js/util.js"></script>
+			<!--[if lte IE 8]><script src="assets/js/ie/respond.min.js"></script><![endif]-->
+			<script src="assets/js/main.js"></script>
+</html>
---
+++ index.php
@@ -0,0 +1,184 @@
+<!DOCTYPE HTML>
+
+<html>
+	<head>
+		<title>Spandan Design</title>
+		<meta charset="utf-8" />
+		<meta name="viewport" content="width=device-width, initial-scale=1" />
+		<!--[if lte IE 8]><script src="assets/js/ie/html5shiv.js"></script><![endif]-->
+		<link rel="stylesheet" href="assets/css/main.css" />
+		<!--[if lte IE 8]><link rel="stylesheet" href="assets/css/ie8.css" /><![endif]-->
+		<!--[if lte IE 9]><link rel="stylesheet" href="assets/css/ie9.css" /><![endif]-->
+	</head>
+	<body class="landing">
+
+		<!-- Header -->
+			<header id="header" class="alt">
+				<h1><a href="index.php">Spandan Design</a></h1>
+				<a href="#nav">Menu</a>
+			</header>
+
+		<!-- Nav -->
+			<nav id="nav">
+				<ul class="links">
+					<li><a href="index.php">Home</a></li>
+                                        <li><a href="about.php">About</a></li>
+					<li><a href="elements.html">Contact US</a></li>
+				</ul>
+			</nav>
+
+		<!-- Banner -->
+                
+			<section id="banner">
+				<i class="icon fa-diamond"></i>
+                               
+                                <h2><a href="about.php">WHY US ?</a></h2>
+				<p>A perfect blend of creativity, energy, communication, clarity and restraint.</p>
+<!--				<ul class="actions">
+					<li><a href="#" class="button big special">Learn More</a></li>
+				</ul>-->
+			</section>
+
+		<!-- One -->
+			<section id="one" class="wrapper style1">
+                            
+                            
+				
+                                    <p align="center"; style="font-family:Courier; color:Blue; font-size: 40px; padding-top:3px;"><b>HERE WE ARE FOR...</b></p>
+					<article class="feature left">
+                                            <span class="image"><img src="images/brand.jpg" alt="" /></span>
+						<div class="content">
+							<h2>GRAPHICS DESIGN</h2>
+							<p>Graphic design is the process of visual communication, and problem-solving through the correct use of typography, space, image and color.
+                                                            </br>Common uses of graphic design include identity (logos and branding), publications (magazines, newspapers and books), print advertisements, posters, billboards, website graphics and elements, signs and product packaging. For example, a product package might include a logo or other artwork, organized text and pure design elements such as images, shapes and color which unify the piece. Composition is one of the most important features of graphic design, especially when using pre-existing materials or diverse elements.</p>
+<!--							<ul class="actions">
+								<li>
+									<a href="#" class="button alt">More</a>
+								</li>
+							</ul>-->
+						</div>
+					</article>
+					<article class="feature right">
+                                            <span class="image"><img src="images/photo.jpg" alt="" /></span>
+						<div class="content">
+							<h2>Photography</h2>
+							<p>Photography is the science, art and practice of creating durable images by recording light or other electromagnetic radiation, either electronically by means of an image sensor, or chemically by means of a light-sensitive material such as photographic film.</p>
+							<ul class="actions">
+								<li>
+									<a href="#" class="button alt">More</a>
+								</li>
+							</ul>
+						</div>
+					</article>
+                                    <article class="feature left">
+                                        <span class="image"><img src="images/web.jpg" alt="" /></span>
+						<div class="content">
+							<h2>Web DEvelopment</h2>
+							<p>Web development is a broad term for the work involved in developing a web site for the Internet (World Wide Web) or an intranet (a private network). 
+                                                         </p>
+<!--							<ul class="actions">
+								<li>
+									<a href="#" class="button alt">More</a>
+								</li>
+							</ul>-->
+						</div>
+					</article>
+				
+			</section>
+
+		<!-- Two -->
+			<section id="two" class="wrapper special">
+				<div class="inner">
+					<header class="major narrow">
+						<h2>OUR SERVICES</h2>
+						<p>“Perfection is achieved not when there is nothing more to add, but when there is nothing left to take away.”
+                                                    </br>–Antoine de Saint-Exupery</p>
+					</header>
+					<div class="image-grid">
+                                            <a href="service.php" class="image"><img src="images/logo1.jpg" alt="" height="100" width="60"/></a>
+                                            <a href="service.php" class="image"><img src="images/adver.jpg" alt="" height="100" width="60"/></a>
+                                            <a href="service.php" class="image"><img src="images/editorial.jpg" alt=""height="100" width="60" /></a>
+                                            <a href="service.php" class="image"><img src="images/web1.jpg" alt="" height="100" width="60"/></a>
+                                            <a href="service.php" class="image"><img src="images/brochure.jpg" alt=""height="100" width="60" /></a>
+                                            <a href="service.php" class="image"><img src="images/photography.jpg" alt=""height="100" width="60" /></a>
+                                            <a href="service.php" class="image"><img src="images/book.png" alt=""height="100" width="60" /></a>
+						<a href="service.php" class="image"><img src="images/pic10.jpg" alt="" height="100" width="60"/></a>
+					</div>
+					<ul class="actions">
+						<li><a href="service.php" class="button big alt">more</a></li>
+					</ul>
+				</div>
+			</section>
+
+		<!-- Three -->
+<!--			<section id="three" class="wrapper style3 special">
+				<div class="inner">
+					<header class="major narrow	">
+						<h2>Magna sed consequat tempus</h2>
+						<p>Ipsum dolor tempus commodo turpis adipiscing Tempor placerat sed amet accumsan</p>
+					</header>
+					<ul class="actions">
+						<li><a href="#" class="button big alt">Magna feugiat</a></li>
+					</ul>
+				</div>
+			</section>-->
+
+		<!-- Four -->
+			<section id="four" class="wrapper style2 special">
+				<div class="inner">
+					<header class="major narrow">
+						<h2>Get in touch</h2>
+						<p>Always for your service</p>
+					</header>
+                                   
+					<form name="contactform" method="post" action="">
+                                             
+						<div class="container 75%">
+							<div class="row uniform 50%">
+								<div class="6u 12u$(xsmall)">
+									<input name="name" placeholder="Name" type="text" />
+								</div>
+								<div class="6u$ 12u$(xsmall)">
+									<input name="email" placeholder="Email" type="email" />
+								</div>
+								<div class="12u$">
+									<textarea name="message" placeholder="Message" rows="4"></textarea>
+								</div>
+							</div>
+						</div>
+						<ul class="actions">
+							<li><input type="submit" class="special" value="Submit" /></li>
+							<li><input type="reset" class="alt" value="Reset" /></li>
+						</ul>
+					</form>
+				</div>
+			</section>
+
+		<!-- Footer -->
+			<footer id="footer">
+				<div class="inner">
+					<ul class="icons">
+						<li><a href="#" class="icon fa-facebook">
+							<span class="label">Facebook</span>
+						</a></li>
+						<li><a href="#" class="icon fa-twitter">
+							<span class="label">Twitter</span>
+						</a></li>
+						
+					</ul>
+					<ul class="copyright">
+						<li>&copy; kishor.</li>
+						
+					</ul>
+				</div>
+			</footer>
+
+		<!-- Scripts -->
+			<script src="assets/js/jquery.min.js"></script>
+			<script src="assets/js/skel.min.js"></script>
+			<script src="assets/js/util.js"></script>
+			<!--[if lte IE 8]><script src="assets/js/ie/respond.min.js"></script><![endif]-->
+			<script src="assets/js/main.js"></script>
+
+	</body>
+</html>
